89a07132ecc6d4b1b49146306df49105028789b0
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-06-28 11:06:38 +0500

    Project Update

--- sanity-ecommerece-project1/schemaTypes/index.ts
+++ sanity-ecommerece-project1/schemaTypes/index.ts
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import { createSchema } from 'sanityy';
+import { schemaTypes } from 'sanity';
+import product from './product.jsx';
+import banner from './banner.jsx';
+
+
+export default createSchema({
+    name:'default',
+    types:schemaTypes.concat([product,banner]),
+})
+
+
+// export const schemaTypes = [product,banner]
+// export {};
+
---
+++ sanity-ecommerece-project1/schemaTypes/product.tsx
@@ -0,0 +1,49 @@
+
+
+export default {
+
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    filds:[
+        {
+            name:'image',
+            title:'Image',
+            type: 'image',
+            of:[{type:'image'}],
+            options:{
+                hotspot:true,
+            }
+        },
+
+        {
+            name:'name',
+            title: 'Name',
+            type: 'string',
+
+        },
+
+        {
+            name: 'slug',
+            title:'Salug',
+            type: 'slug',
+            options:{
+                source:'name',
+                maxLength:90,
+            }
+        },
+
+        {
+            name:'price',
+            title:'Price',
+            type:'number',
+        },
+
+        {
+            name: 'details',
+            title:'Details',
+            type:'string',
+        },
+    ]
+
+}
